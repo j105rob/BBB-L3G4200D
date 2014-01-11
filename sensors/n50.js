5e897d8a4fbab9fdba63aefe130a9c263e5e3938
j105rob <j105.rob@gmail.com>
2014-01-11 08:00:50 -0500

    Added more test code

--- sensors/n50.js
+++ sensors/n50.js
@@ -8,9 +8,11 @@ if ( typeof exports === 'undefined')
  */
 
 var HID = require('node-hid');
+var hidstream = require('hidstream');
 var devices = HID.devices();
 //assumption is that there is only one HID on the gun attached to USB and it is the n50!!
-var n50 = new HID.HID(devices[0].path);
+var n50 = new hidstream.device(devices[0].path);
+var n50Raw = new HID.HID(devices[0].path);
 var registers = {
 
 };
@@ -18,8 +20,16 @@ var state = function(observer){
 	return observer(registers);
 }
 var initialize = function() {
-	n50.on("data", function(data) {
-		console.log(data);
+	n50Raw.on("data", function(data) {
+		var seg1 = data.readInt8(0);
+		var seg2 = data.readInt8(1);
+		var seg3 = data.readInt8(2);
+		var seg4 = data.readInt8(3);
+		var tot = seg1+seg2+seg3+seg4;
+
+		
+		console.log(seg1,seg2,seg3,seg4);
+		console.log("****************************");
 	});
 };
 var setState = function() {
